99c10118892329c38675fb8699f0500f8f8daa42
Dave Rouch <dave.rouch@gmail.com>
2019-09-21 14:02:36 +0100

    Updates to html etc

--- cam.sh
+++ cam.sh
@@ -4,7 +4,7 @@
 # Pre & Post processing
 #
 #echo sh+ `date +"%F %T"` `df -h / --output=avail | tr -d '\n'` >> cam.log
-echo sh+ `date +"%F %T"` `df -h / --output=avail | tail -1` Free  >> cam.log
+echo sh+ `date +"%F %T"` `df -h / --output=avail | tail -1` Free  >> /home/pi/cam.log
 python3 cam.py
-echo sh- `date +"%F %T"` `df -h / --output=avail | tail -1` Free  >> cam.log
+echo sh- `date +"%F %T"` `df -h / --output=avail | tail -1` Free  >> /home/pi/cam.log
 exit 0
